7b81ab52a7cef0dade8a456597fc30f10e752cb9
phuocpjnh <manguyenphuocbinh2712@gmail.com>
2024-03-21 14:10:00 +0700

    chanvlz

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
+const mongoose = require("mongoose");
 dotenv.config()
 
 const app = express()
@@ -7,6 +8,19 @@ const port = process.env.PORT || 3001
 
 app.get('/', (req, res) => {
     res.send('Hello con c')
+})
+console.log('process.env.MONGO_DB', process.env.MONGO_DB)
+mongoose.connect(`mongodb+srv://abdulsalam17:${process.env.MONGO_DB}@cluster0.o9ibzxb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+    .then(() => {
+         console.log('Connect Db success!')
+    })
+    .catch((err) => {
+         console.log(err)   
+    })
+
+
+.catch((err) => {
+    console.log(err)
 })
 
 app.listen(port, () => {
