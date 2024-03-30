0c09767bc264ec8eba5fda2f4ccf1355f4566365
TinhPhoMai <trong.tinh1922002@gmail.com>
2024-03-30 15:08:54 +0700

    cart2

--- front-end/main.js
+++ front-end/main.js
@@ -45,19 +45,18 @@ var swiper = new Swiper(".mySwiper", {
     const iconUser = document.querySelector('.bx-user');
 
     iconUser.addEventListener('click', function() {
-      // Hiển thị form đăng nhập và ẩn form đăng ký
+     
       loginForm.style.display = 'block';
       registerForm.style.display = 'none';
-      // Hiển thị modal
+     
       loginModal.style.display = 'block';
   });
-    // Ẩn form đăng ký khi trang được tải
+    
     registerForm.style.display = 'none';
 
-    // Thêm sự kiện click cho các nút chuyển đổi
+    
     switchButtons.forEach(function(button) {
         button.addEventListener('click', function() {
-            // Nếu đang hiển thị form đăng ký, ẩn nó và hiển thị form đăng nhập, và ngược lại
             if (loginForm.style.display === 'none') {
                 loginForm.style.display = 'block';
                 registerForm.style.display = 'none';
@@ -68,14 +67,12 @@ var swiper = new Swiper(".mySwiper", {
         });
     });
 
-    // Thêm sự kiện click cho nút "Đăng ký" trong form đăng nhập
     const registerButtonInLoginForm = document.querySelector('#loginForm .auth-form__swtich-btn'); 
     registerButtonInLoginForm.addEventListener('click', function() {
         loginForm.style.display = 'none';
         registerForm.style.display = 'block';
     });
 
-    // Thêm sự kiện click cho nút "Đăng nhập" trong form đăng ký
     const loginButtonInRegisterForm = document.querySelector('#registerForm .auth-form__swtich-btn');
     loginButtonInRegisterForm.addEventListener('click', function() {
         loginForm.style.display = 'block';
@@ -85,14 +82,27 @@ var swiper = new Swiper(".mySwiper", {
    
 
     modalOverlay.addEventListener('click', function(event) {
-      // Kiểm tra xem click có diễn ra bên ngoài cả hai form không
       if (loginModal.contains(event.target)) {
-          // Ẩn cả form đăng nhập và đăng ký khi click bên ngoài form modal
           loginModal.style.display = 'none';
          
       }
   });
 
+  document.addEventListener('DOMContentLoaded', function() {
+    const card = document.querySelector('.card');
+    const closeShopingButton = document.querySelector('.closeShoping');
+    const showCardButton = document.getElementById('showCardButton');
+
+    // Bắt sự kiện click vào nút "Close" để ẩn `.card`
+    closeShopingButton.addEventListener('click', function() {
+        card.style.display = 'none'; // Ẩn `.card`
+    });
+
+    // Bắt sự kiện click vào nút "Mở Card" để hiển thị `.card`
+    showCardButton.addEventListener('click', function() {
+        card.style.display = 'block'; // Hiển thị `.card`
+    });
+  });
    
 });
 
